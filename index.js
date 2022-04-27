8463346220c17393241b4a2654dbf253fbbac748
Susan Minott <susan_minott@yahoo.com>
2022-04-27 11:33:25 -0400

    commit tree 1ba88ca5a92c7ff08eab03d03e3a84d5a5d73836

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+// 1st code
+function saturdayFun(defaultActivity1 = "roller-skate") {
+    return `This Saturday, I want to ${defaultActivity1}!`;
+    };
+
+    saturdayFun();
+    saturdayFun("bathe my dog");
+  
+// 2nd code
+function mondayWork(defaultActivity2 = 'go to the office') {
+    return `This Monday, I will ${defaultActivity2}.`;
+};
+    mondayWork('work from home');
+
+// 3rd code
+// function wrapAdjective(defaultOuter = 'My genius!') {
+//     function(defaultInner = 'special'){
+//         return `You are ${defaultInner}`
+
+//     };
+// };
+ const wrapAdjective = (flair = '*') => {
+     return (str = 'special') => {
+        return `You are ${flair}${str}${flair}!`
+     }
+ }
